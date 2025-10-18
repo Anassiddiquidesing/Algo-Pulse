e480162b731b2d248d1446be55ad98367f7c8cf3
Saba Siddiqui <misssabasiddiqui@gmail.com>
2025-10-18 09:24:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4375966934-1e6e0",
+  "appId": "1:1045097925059:web:30f87382e5a549bf7ad576",
+  "apiKey": "AIzaSyAD8GtsuG0i6SjLV2gPeirw55_7DaLxct4",
+  "authDomain": "studio-4375966934-1e6e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1045097925059"
+};
